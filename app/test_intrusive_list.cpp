a1242ba7532385a15b16b197b8de700e452814c0
rnezil <rylandnezil@gmail.com>
2023-07-04 16:28:48 -0700

    Modifies iterator and const_iterator classes to reference an object of type value_type instead of a list_hook object
    Adds default constructor
    	initializes size_ to 0
    
    	initializes sentinel_.next_ and sentinel_.prev_ to both
    	point at sentinel_, thus indicating an empty list
    
    Adds destructor
    	assigns sentinel_.next_ and sentinel_.prev_ to nullptr
    
    Adds move constructor
    	move constructor guarantees that the source list will be left
    	empty
    
    Adds move assignment operator
    	move assignment operator guarantees that the sources list
    	will be left empty
    
    Adds swap member function
    	analogous to std::swap(list1, list2)
    
    Adds size member function
    	returns size_
    
    Adds insert member function
    	insert takes an iterator and a value_type as argument and
    	inserts the value_type object into the list in the location
    	that the object pointed to by the iterator previously
    	occupied
    
    	insert returns an iterator referring to the element that was
    	inserted
    
    Adds erase member function
    	erase takes an iterator as its only argument and removes
    	the element referred to by that iterator from the list
    
    Adds push_back member function
    	push_back takes a reference to a value_type object as its
    	only parameter and inserts that object at the end of the
    	list
    
    Adds pop_back member function
    	pop_back erases the element at the end of the list
    
    Adds back member function
    	back returns a reference to the element at the end of the list
    
    Adds begin member function
    	begin returns an interator referring to the element at the
    	beginning of the list
    
    Adds end member function
    	end returns an iterator referring to the fictitious one-past-
    	the-end element at the end of the list

--- app/test_intrusive_list.cpp
+++ app/test_intrusive_list.cpp
@@ -10,6 +10,11 @@ struct Widget {
 	ri::list_hook hook;
 };
 
+std::ostream& operator<<(std::ostream& out, const Widget& w){
+	out << w.value;
+	return out;
+}
+
 using iterator = ri::list<Widget, &Widget::hook>::intrusive_list_iterator;
 using const_iterator = ri::list<Widget, &Widget::hook>::const_intrusive_list_iterator;
 
@@ -17,8 +22,20 @@ int main()
 {
 	std::vector<Widget> storage;
 	storage.push_back(Widget(42));
+	for( int i = 0; i < 10; ++i )
+		storage.push_back(Widget(i*5));
 	ri::list<Widget, &Widget::hook> values;
-	iterator a;
-	const_iterator b;
+	for(auto&& i : storage) {
+		values.push_back(i);
+	}
+	values.print();
+	std::cout << "\n\n";
+	auto jank = values.begin();
+	std::cout << *jank << "\n\n\n";
+	// Point to 3
+	std::cout << *++++++++jank << "\n\n\n";
+	Widget foo(69);
+	std::cout << *values.insert(jank, foo) << "\n\n\n";
+	values.print();
 	return 0;
 }
--- include/ra/intrusive_list.hpp
+++ include/ra/intrusive_list.hpp
@@ -1,6 +1,8 @@
 #include "ra/parent_from_member.hpp"
 #include <boost/iterator/iterator_facade.hpp>
 #include <cstddef>
+#include <utility>
+#include <iostream>
 
 namespace ra::intrusive{
 
@@ -8,7 +10,7 @@ namespace ra::intrusive{
 // instance, use parent_from_member function
 class list_hook{
 public:
-	list_hook(): next_ {nullptr}, prev_ {nullptr} {}
+	list_hook(): next_ {this}, prev_ {this} {}
 
 	list_hook(const list_hook&): list_hook() {}
 
@@ -62,97 +64,113 @@ public:
 	class intrusive_list_iterator
 		: public boost::iterator_facade<
 		  intrusive_list_iterator,
-		  list_hook,
+		  value_type,
 		  boost::bidirectional_traversal_tag>
 	{
 		public:
 			intrusive_list_iterator()
-				: base_ {nullptr}
+				: base_(nullptr)
 			{}
 
-			explicit intrusive_list_iterator( list_hook* le_hook )
-				: base_ {le_hook}
-			{}
-
-			intrusive_list_iterator( const const_intrusive_list_iterator& other )
-				: base_ {other.base_}
+			explicit intrusive_list_iterator( value_type* element )
+				: base_(element)
 			{}
 
 		private:
 			friend class boost::iterator_core_access;
 			friend class const_intrusive_list_iterator;
 
-			void increment() { base_ = base_->next_; }
+			void increment() {
+				base_ = ra::util::parent_from_member<
+					value_type, list_hook>(
+							(base_->*hook_ptr).next_, hook_ptr);
+			}
 
-			void decrement() { base_ = base_->prev_; }
+			void decrement() {
+				base_ = ra::util::parent_from_member<
+					value_type, list_hook>(
+							(base_->*hook_ptr).prev_, hook_ptr);
+			}
 
 			bool equal( const intrusive_list_iterator& other ) const
 			{
-				return (this->base_->next_ == other.base_->next_) &&
-					(this->base_->prev_ == other.base_->prev_);
+				return base_ == other.base_;
 			}
 			
 			bool equal( const const_intrusive_list_iterator& other ) const
 			{
-				return (this->base_->next_ == other.base_->next_) &&
-					(this->base_->prev_ == other.base_->prev_);
-			}
-	
-			T& dereference() const
-			{
-				return *ra::util::parent_from_member<list_hook, hook_ptr>(
-						base_, hook_ptr);
+				return base_ == other.base_;
 			}
 
-			list_hook* base_;
+			bool equal( const value_type* p ) const
+			{
+				return base_ == p;
+			}
+	
+			value_type& dereference() const
+			{
+				return *base_;
+			}
+
+			value_type* base_;
 	};
 
 	class const_intrusive_list_iterator
 		: public boost::iterator_facade<
 		  const_intrusive_list_iterator,
-		  const list_hook,
+		  const value_type,
 		  boost::bidirectional_traversal_tag>
 	{
 		public:
 			const_intrusive_list_iterator()
-				: base_ {nullptr}
+				: base_(nullptr)
 			{}
 
-			explicit const_intrusive_list_iterator( const list_hook* le_hook )
-				: base_ {le_hook}
+			explicit const_intrusive_list_iterator( value_type* element )
+				: base_(element)
 			{}
 
 			const_intrusive_list_iterator( const intrusive_list_iterator& other )
-				: base_ {other.base_}
+				: base_(other.base_)
 			{}
 
 		private:
 			friend class boost::iterator_core_access;
 			friend class intrusive_list_iterator;
 
-			void increment() { base_ = base_->next_; }
+			void increment() {
+				base_ = ra::util::parent_from_member<
+					value_type, list_hook>(
+							(base_->*hook_ptr).next_, hook_ptr);
+			}
 
-			void decrement() { base_ = base_->prev_; }
+			void decrement() {
+				base_ = ra::util::parent_from_member<
+					value_type, list_hook>(
+							(base_->*hook_ptr).prev_, hook_ptr);
+			}
 
 			bool equal( const const_intrusive_list_iterator& other ) const
 			{
-				return (this->base_->next_ == other.base_->next_) &&
-					(this->base_->prev_ == other.base_->prev_);
+				return base_ == other.base_;
 			}
 			
 			bool equal( const intrusive_list_iterator& other ) const
 			{
-				return (this->base_->next_ == other.base_->next_) &&
-					(this->base_->prev_ == other.base_->prev_);
-			}
-	
-			const T& dereference() const
-			{
-				return *ra::util::parent_from_member<const list_hook, hook_ptr>(
-						base_, hook_ptr);
+				return base_ == other.base_;
 			}
 
-			const list_hook* base_;
+			bool equal( const value_type* p ) const
+			{
+				return base_ == p;
+			}
+	
+			const value_type& dereference() const
+			{
+				return *base_;
+			}
+
+			const value_type* base_;
 	};
 
 	// Default constructor
@@ -165,47 +183,173 @@ public:
 	}
 
 	// Move constructor
-	list(list&& other);
+	list(list&& other){
+		sentinel_.next_ = other.sentinel_.next_;
+		sentinel_.prev_ = other.sentinel_.prev_;
+		size_ = other.size_;
+		other.sentinel_.next_ = &other.sentinel_;
+		other.sentinel_.prev_ = &other.sentinel_;
+	}
 
 	// Move assignment operator
-	list& operator=(list&& other);
+	list& operator=(list&& other){
+		sentinel_.next_ = other.sentinel_.next_;
+		sentinel_.prev_ = other.sentinel_.prev_;
+		size_ = other.size_;
+		other.sentinel_.next_ = &other.sentinel_;
+		other.sentinel_.prev_ = &other.sentinel_;
+
+		return *this;
+	}	
 
 	// Do not allow the copying of lists
 	list(const list&) = delete;
 	list& operator=(const list&) = delete;
 
 	// Swap function
-	void swap(list& x);
+	void swap(list& x){
+		auto tmpn = sentinel_.next_;
+		auto tmpp = sentinel_.next_;
+		auto tmps = size_;
+		sentinel_.next_ = x.sentinel_.next_;
+		sentinel_.prev_ = x.sentinel_.prev_;
+		size_ = x.size_;
+		x.sentinel_.next_ = tmpn;
+		x.sentinel_.prev_ = tmpp;
+		x.size_ = tmps;
+	}
 
 	// Returns list size
-	size_type size() const;
+	size_type size() const { return size_; }
 
 	// Insert function
-	iterator insert(iterator pos, value_type& value);
+	iterator insert(iterator pos, value_type& value){
+		// If inserting at front of list, update
+		// sentinel node
+		if( pos == begin() )
+			sentinel_.next_ = &((*pos).*hook_ptr);
+
+		// Front hook new item into item being
+		// bumped down
+		(value.*hook_ptr).next_ = &((*pos).*hook_ptr);
+
+		// Rear hook new item into prev of item
+		// being bumped down
+		(value.*hook_ptr).prev_ = ((*pos).*hook_ptr).prev_;
+
+		// Front hook prev of item being bumped
+		// down into new item
+		(((*pos).*hook_ptr).prev_)->next_ = &(value.*hook_ptr);
+
+		// Rear hook item being bumped down into
+		// new item
+		((*pos).*hook_ptr).prev_ = &(value.*hook_ptr);
+
+		// Adjust size
+		++size_;
+
+		return --pos;
+	}
 
 	// Erase function
-	iterator erase(iterator pos);
+	iterator erase(iterator pos){
+		// Front hook item in front of pos
+		// to item behind pos
+		((*pos).*hook_ptr).prev_->next_ = ((*pos).*hook_ptr).next_;
+
+		// Rear hook item behind pos to item
+		// in front of pos
+		((*pos).*hook_ptr).next_->prev_ = ((*pos).*hook_ptr).prev_;
+
+		--size_;
+	}
 
 	// Push back function
-	void push_back(value_type& x);
+	void push_back(value_type& x){
+		// Rear hook new item to end of
+		// list item
+		(x.*hook_ptr).prev_ = sentinel_.prev_;
+
+		// Front hook end of list item to
+		// new item
+		sentinel_.prev_->next_ = &(x.*hook_ptr);
+		
+		// Rear hook sentinel node to new
+		// item
+		sentinel_.prev_ = &(x.*hook_ptr);
+
+		// Front hook new item to item at
+		// the beginning of the list
+		(x.*hook_ptr).next_ = sentinel_.next_;
+
+		// Rear hook item at the begiining
+		// of the list to new item
+		(sentinel_.next_)->prev_ = &(x.*hook_ptr);
+
+		// Increment size
+		++size_;
+	}
 
 	// Pop back function
-	void pop_back();
+	void pop_back(){
+		erase(iterator(&back()));
+		--size_;
+	}
 
 	// Get reference for last element
-	reference back();
-	const_reference back() const;
+	reference back(){
+		iterator getref(ra::util::parent_from_member<value_type, list_hook>(
+					sentinel_.prev_, hook_ptr));
+		return *getref;
+	}
+
+	const_reference back() const{
+		const_iterator getref(ra::uitil::parent_from_member<value_type, list_hook>(
+					sentinel_.prev_, hook_ptr));
+	}
 
 	// Clear list
-	void clear();
+	void clear(){
+		sentinel_.next_ = sentinel_;
+		sentinel_.prev_ = sentinel_;
+		size_ = 0;
+	}
+
+	// Get const begin iterator
+	const_iterator begin() const { 
+		return const_iterator(ra::util::parent_from_member<value_type, list_hook>(
+				sentinel_.next_, hook_ptr));
+	}
 
 	// Get begin iterator
-	const_iterator begin() const;
-	iterator begin();
+	iterator begin() {
+		return iterator(ra::util::parent_from_member<value_type, list_hook>(
+				sentinel_.next_, hook_ptr));
+	}
+
+	// Get const end iterator
+	const_iterator end() const {
+		return const_iterator(ra::util::parent_from_member<value_type, list_hook>(
+				sentinel_.prev_, hook_ptr));
+	}
 
 	// Get end iterator
-	const_iterator end() const;
-	iterator end();
+	iterator end() {
+		return nullptr;
+		//return iterator(ra::util::parent_from_member<value_type, list_hook>(
+		//		sentinel_.prev_, hook_ptr));
+	}
+
+	// Debugging function
+	void print() const {
+		const_iterator iter = begin();
+		std::cout << *iter << '\n';
+		++iter;
+		while( iter != begin() ){
+			std::cout << *iter << '\n';
+			++iter;
+		}
+	}
 
 private:
 	size_type size_;
